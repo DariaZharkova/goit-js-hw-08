f2add7abdf840e6b0b02a5fe73046d0da5e41ed1
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2023-12-02 19:10:31 +0200

    add task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const formData = {};
+const LS_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LS_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  if (!form.email.value || !form.message.value) {
+    alert('Please, fill in all fields!');
+  } else {
+    console.log(JSON.parse(localStorage.getItem(LS_KEY)));
+    localStorage.removeItem(LS_KEY);
+    evt.currentTarget.reset();
+  }
+}
+// function PopulateForm() {
+//   const currentInfo = JSON.parse(localStorage.getItem(LS_KEY));
+//   if (currentInfo) {
+//     form.email.value = currentInfo.email;
+//     form.message.value = currentInfo.message;
+//   }
+// }
+// PopulateForm();
+
+function load(key) {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
+
+const storageData = load(LS_KEY);
+if (storageData) {
+  form.email.value = storageData.email;
+  form.message.value = storageData.message;
+}
